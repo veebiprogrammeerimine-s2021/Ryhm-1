3ec7b53620939b0b87ea377818e307cfcaea7806
Andrus Rinde <andrusrinde@users.noreply.github.com>
2021-11-09 08:25:27 +0200

    Üheksas kodutöö tehtud

---
+++ 10_tund/fnc_gallery.php
@@ -0,0 +1,196 @@
+<?php
+	$database = "if21_rinde";
+
+	function show_latest_public_foto(){
+        $photo_html = null;
+        $privacy = 3;
+        $conn = new mysqli($GLOBALS["server_host"], $GLOBALS["server_user_name"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		$conn->set_charset("utf8");
+        $stmt = $conn->prepare("SELECT filename, alttext FROM vpr_photos WHERE id = (SELECT MAX(id) FROM vpr_photos WHERE privacy = ? AND deleted IS NULL)");
+        echo $conn->error;
+        $stmt->bind_param("i", $privacy);
+        $stmt->bind_result($filename_from_db, $alttext_from_db);
+        $stmt->execute();
+        if($stmt->fetch()){
+            //<img src="kataloog/filename" alt="alttekst">
+            $photo_html = '<img src="' .$GLOBALS["photo_normal_upload_dir"] .$filename_from_db .'" alt="';
+            if(empty($alttext_from_db)){
+                $photo_html .= "Üleslaetud foto";
+            } else {
+                $photo_html .= $alttext_from_db;
+            }
+            $photo_html .= '">' ."\n";
+        } else {
+            $photo_html = "<p>Kahjuks avalikke fotosid üles leatud pole!</p> \n";
+        }
+        $stmt->close();
+		$conn->close();
+		return $photo_html;
+    }
+    
+    function read_public_photo_thumbs($privacy, $page, $limit){
+        $photo_html = null;
+        $skip = ($page - 1) * $limit;
+        $conn = new mysqli($GLOBALS["server_host"], $GLOBALS["server_user_name"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		$conn->set_charset("utf8");
+        //$stmt = $conn->prepare("SELECT filename, alttext FROM vpr_photos WHERE privacy >= ? AND deleted IS NULL");
+        //$stmt = $conn->prepare("SELECT filename, alttext FROM vpr_photos WHERE privacy >= ? AND deleted IS NULL ORDER BY id DESC");
+        $stmt = $conn->prepare("SELECT filename, created, alttext FROM vpr_photos WHERE privacy >= ? AND deleted IS NULL ORDER BY id DESC LIMIT ?, ?");
+        echo $conn->error;
+        $stmt->bind_param("iii", $privacy, $skip, $limit);
+        $stmt->bind_result($filename_from_db, $created_from_db, $alttext_from_db);
+        $stmt->execute();
+        while($stmt->fetch()){
+            //<div>
+            //<img src="kataloog/filename" alt="alttekst">
+            //....
+            //</div>
+            $photo_html .= '<div class="thumbgallery">' ."\n";
+            $photo_html .= '<img src="' .$GLOBALS["photo_thumbnail_upload_dir"] .$filename_from_db .'" alt="';
+            if(empty($alttext_from_db)){
+                $photo_html .= "Üleslaetud foto";
+            } else {
+                $photo_html .= $alttext_from_db;
+            }
+            $photo_html .= '" class="thumbs">' ."\n";
+            $gallery_html .= "<p>Lisatud: " .date_to_est_format($created_from_db) ."</p> \n";
+            $photo_html .= "</div> \n";
+        }
+        if(empty($photo_html)){
+            $photo_html = "<p>Kahjuks avalikke fotosid üles laetud pole!</p> \n";
+        }
+        $stmt->close();
+		$conn->close();
+		return $photo_html;
+    }
+    
+    function count_public_photos($privacy){
+        $photo_count = 0;
+        $conn = new mysqli($GLOBALS["server_host"], $GLOBALS["server_user_name"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		$conn->set_charset("utf8");
+        $stmt = $conn->prepare("SELECT COUNT(id) FROM vpr_photos WHERE privacy >= ? AND deleted IS NULL");
+        echo $conn->error;
+        $stmt->bind_param("i", $privacy);
+        $stmt->bind_result($count_from_db);
+        $stmt->execute();
+        if($stmt->fetch()){
+            $photo_count = $count_from_db;
+        }
+        $stmt->close();
+		$conn->close();
+		return $photo_count;
+    }
+    
+    function read_own_photo_thumbs($page, $limit){
+        $photo_html = null;
+        $skip = ($page - 1) * $limit;
+        $conn = new mysqli($GLOBALS["server_host"], $GLOBALS["server_user_name"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		$conn->set_charset("utf8");
+        $stmt = $conn->prepare("SELECT id, filename, created, alttext FROM vpr_photos WHERE userid = ? AND deleted IS NULL ORDER BY id DESC LIMIT ?, ?");
+        echo $conn->error;
+        $stmt->bind_param("iii", $_SESSION["user_id"], $skip, $limit);
+        $stmt->bind_result($id_from_db, $filename_from_db, $created_from_db, $alttext_from_db);
+        $stmt->execute();
+        while($stmt->fetch()){
+            //<div>
+            //<a href="edit_own_photo.php?photo=x>
+            //<img src="kataloog/filename" alt="alttekst">
+            //....
+            //</a>
+            //</div>
+            $photo_html .= '<div class="thumbgallery">' ."\n";
+            $photo_html .= '<a href="edit_own_photo.php?photo=' .$id_from_db .'">';
+            $photo_html .= '<img src="' .$GLOBALS["photo_thumbnail_upload_dir"] .$filename_from_db .'" alt="';
+            if(empty($alttext_from_db)){
+                $photo_html .= "Üleslaetud foto";
+            } else {
+                $photo_html .= $alttext_from_db;
+            }
+            $photo_html .= '" class="thumbs">' ."\n";
+            $photo_html .= "</a>";
+            $gallery_html .= "<p>Lisatud: " .date_to_est_format($created_from_db) ."</p> \n";
+            $photo_html .= "</div> \n";
+        }
+        if(empty($photo_html)){
+            $photo_html = "<p>Kahjuks avalikke fotosid üles laetud pole!</p> \n";
+        }
+        $stmt->close();
+		$conn->close();
+		return $photo_html;
+    }
+    
+    function count_own_photos(){
+        $photo_count = 0;
+        $conn = new mysqli($GLOBALS["server_host"], $GLOBALS["server_user_name"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		$conn->set_charset("utf8");
+        $stmt = $conn->prepare("SELECT COUNT(id) FROM vpr_photos WHERE userid = ? AND deleted IS NULL");
+        echo $conn->error;
+        $stmt->bind_param("i", $_SESSION["user_id"]);
+        $stmt->bind_result($count_from_db);
+        $stmt->execute();
+        if($stmt->fetch()){
+            $photo_count = $count_from_db;
+        }
+        $stmt->close();
+		$conn->close();
+		return $photo_count;
+    }
+    
+    //pildi andmete lugemine muutmiseks
+	function read_own_photo($photo){
+		$photo_data = [];
+		$conn = new mysqli($GLOBALS["server_host"], $GLOBALS["server_user_name"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		$conn->set_charset("utf8");
+        $stmt = $conn->prepare("SELECT filename, alttext, privacy FROM vpr_photos WHERE id = ? AND userid = ? AND deleted IS NULL");
+		echo $conn->error;
+        $stmt->bind_param("ii", $photo, $_SESSION["user_id"]);
+        $stmt->bind_result($filename_from_db, $alttext_from_db, $privacy_from_db);
+		$stmt->execute();
+		if($stmt->fetch()){
+            array_push($photo_data, true);
+			array_push($photo_data, $filename_from_db);
+			array_push($photo_data, $alttext_from_db);
+			array_push($photo_data, $privacy_from_db);
+        } else {
+			array_push($photo_data, false);
+		}
+		$stmt->close();
+		$conn->close();
+		return $photo_data;
+	}
+	
+	function photo_data_update($photo, $alttext, $privacy){
+		$notice = null;
+		$conn = new mysqli($GLOBALS["server_host"], $GLOBALS["server_user_name"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		$conn->set_charset("utf8");
+        $stmt = $conn->prepare("UPDATE vpr_photos SET alttext = ?, privacy = ? WHERE id = ? AND userid = ?");
+		echo $conn->error;
+        $stmt->bind_param("siii", $alttext, $privacy, $photo, $_SESSION["user_id"]);
+        if($stmt->execute()){
+			$notice = "Andmete muutmine õnnestus!";
+		} else {
+			$notice = "Andmete muutmisel tekkis tõrge!";
+		}
+		$stmt->close();
+		$conn->close();
+		return $notice;
+	}
+	
+	function delete_photo($photo){
+		$notice = null;
+		$conn = new mysqli($GLOBALS["server_host"], $GLOBALS["server_user_name"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		$conn->set_charset("utf8");
+        $stmt = $conn->prepare("UPDATE vpr_photos SET deleted = NOW() WHERE id = ? AND userid = ?");
+		echo $conn->error;
+        $stmt->bind_param("ii", $photo, $_SESSION["user_id"]);
+        if($stmt->execute()){
+			$notice = "ok";
+		} else {
+			$notice = "Foto kustutamisel tekkis tõrge!";
+		}
+		$stmt->close();
+		$conn->close();
+		return $notice;
+	}
+	
+    
---
+++ 10_tund/gallery_public.php
@@ -0,0 +1,64 @@
+<?php
+    //alustame sessiooni
+    session_start();
+    //kas on sisselogitud
+    if(!isset($_SESSION["user_id"])){
+        header("Location: page.php");
+    }
+    //väljalogimine
+    if(isset($_GET["logout"])){
+        session_destroy();
+        header("Location: page.php");
+    }
+	
+    require_once("../../../../config_vp_s2021.php");
+    require_once("fnc_general.php");
+	require_once("fnc_gallery.php");
+    
+    $public_from = 2;
+    $page = 1;
+    $limit = 5;
+    $photo_count = count_public_photos($public_from);
+    //https://greeny.cs.tlu.ee/~rinde/vp2021/Ryhm-1/09_tund/gallery_public.php?page=3
+    if(!isset($_GET["page"]) or $_GET["page"] < 1){
+        $page = 1;
+    } elseif(round($_GET["page"] - 1) * $limit >= $photo_count){
+        $page = ceil($photo_count / $limit);
+    } else {        
+        $page = $_GET["page"];
+    }
+    
+    $to_head = '<link rel="stylesheet" type="text/css" href="style/gallery.css">';
+    require("page_header.php");
+?>
+
+	<h1><?php echo $_SESSION["first_name"] ." " .$_SESSION["last_name"]; ?>, veebiprogrammeerimine</h1>
+	<p>See leht on valminud õppetöö raames ja ei sisalda mingisugust tõsiseltvõetavat sisu!</p>
+	<p>Õppetöö toimus <a href="https://www.tlu.ee/dt">Tallinna Ülikooli Digitehnoloogiate instituudis</a>.</p>
+	<hr>
+    <ul>
+        <li><a href="?logout=1">Logi välja</a></li>
+		<li><a href="home.php">Avaleht</a></li>
+    </ul>
+	<hr>
+    <h2>Avalike fotode galerii</h2>
+    <div>
+    <p>
+    <?php
+        //<span>Eelmine leht</span> | <span>Järgmine leht</span>
+        if($page > 1){
+            echo '<span><a href="?page=' .($page - 1) .'">Eelmine leht</a></span> | ';
+        } else {
+            echo "<span>Eelmine leht</span> | ";
+        }
+        if($page * $limit < $photo_count){
+            echo '<span><a href="?page=' .($page + 1) .'">Järgmine leht</a></span>';
+        } else {
+            echo "<span>Järgmine leht</span>";
+        }
+    ?>
+    </p>
+    <?php echo read_public_photo_thumbs($public_from, $page, $limit); ?>
+    </div>
+</body>
+</html>
