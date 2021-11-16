73296a2db7b9e3b475a89fe8aa300bd3dba56259
Andrus Rinde <andrusrinde@users.noreply.github.com>
2021-11-16 11:22:30 +0200

    Üheteistkümnes tund lõppemas, fotosid saab hinnata

--- 11_tund/javascript/modal.js
+++ 11_tund/javascript/modal.js
@@ -11,12 +11,44 @@ window.onload = function(){
 
 function openModal(e){
     document.querySelector("#modalimg").src = photoDir + e.target.dataset.fn;
+    photoId = e.target.dataset.id;
+    console.log(photoDir + e.target.dataset.fn);
+    document.querySelector("#avgRating").innerHTML = "";
+    for(let i = 1; i < 6; i ++){
+        document.querySelector("#rate" + i).checked = false;
+    }
     document.querySelector("#modalcaption").innerHTML = e.target.alt;
     document.querySelector("#modalarea").style.display = "block";
+    document.querySelector("#storeRating").addEventListener("click", storeRating);
 }
 
 function closeModal(){
     document.querySelector("#modalarea").style.display = "none";
     document.querySelector("#modalimg").src = "../pics/empty.png";
     document.querySelector("#modalcaption").innerHTML = "";
+}
+
+function storeRating(){
+    let rating = 0;
+    for(let i = 1; i < 6; i ++){
+        if(document.querySelector("#rate" + i).checked){
+            rating = i;
+        }
+    }
+    if(rating > 0){
+        //AJAX
+        let webRequest = new XMLHttpRequest();
+        webRequest.onreadystatechange = function(){
+            //kas õnnestus
+            if(this.readyState == 4 && this.status == 200){
+                //asjad, mida teha, kui õnnestus
+                //vastuseks tuleb this.responseText;
+                document.querySelector("#avgRating").innerHTML = "Keskmine hinne: " + this.responseText;
+                document.querySelector("#storeRating").removeEventListener("click", storeRating);
+            }
+        };
+        webRequest.open("GET", "store_photorating.php?photo=" + photoId + "&rating=" + rating, true);
+        webRequest.send();
+        //AJAX lõppes
+    }
 }
